235950f7a45cfdc35cb16d5a0cf9aa69bf985a6a
Kristopher Bredemeier <kb@i22.de>
2018-02-04 17:50:47 +0100

    Adds package info

--- mix.exs
+++ mix.exs
@@ -9,6 +9,9 @@ defmodule SocketProxy.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: packacke(),
+      description: "Proxies socket connection in phoenix channel tests" <>
+                   " through a separate process.",
 
       # Docs
       name: "SocketPoxy",
@@ -35,6 +38,14 @@ defmodule SocketProxy.MixProject do
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp packacke do
+    [
+      maintainers: ["Kristopher Bredemeier"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/kbredemeier/socket_proxy"}
+    ]
+  end
 
   defp deps do
     [
